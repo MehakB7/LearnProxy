97a166fb8da7f36bd7de024a859f95bc0aae02a2
MehakB77 <87482443+MehakB7@users.noreply.github.com>
2023-07-02 18:50:58 +0530

    created the vending machine

---
+++ contracts/VendingMachine2.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract VendingMachineV1 is Initializable {
+    uint public numSodas;
+    address public owner;
+
+    function initialize(uint _numSodas) public initializer {
+        numSodas = _numSodas;
+        owner = msg.sender;
+    }
+
+    function purchaseSoda() public payable {
+        require(msg.value >= 1000 wei, "You must pay 1000 wei for a soda!");
+        numSodas--;
+    }
+
+    function withdrawProfits() public onlyOwner {
+        require(
+            address(this).balance > 0,
+            "Profits must be greater than 0 in order to withdraw!"
+        );
+        (bool success, ) = owner.call{value: address(this).balance}("");
+        require(success);
+    }
+
+    function setNewOwner(address _newOwner) public onlyOwner {
+        owner = _newOwner;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+}
